64ca8e8ef37c0fbe10bb2061e4832829aadd507d
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-20 23:20:09 +0000

    Add two prints

--- LevelOne/MainIsHre.cs
+++ LevelOne/MainIsHre.cs
@@ -14,14 +14,8 @@ namespace LevelOne
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
-        }
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        /// <param name="args">show string</param>
-        private static void Main2(string[] args)
-        {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Bad day?");
+            Console.WriteLine("Bye");
         }
     }
 }
