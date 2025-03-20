2ee972ff8d5b2542b30e4a3e4cdb269d6e495903
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-20 22:15:02 +0000

    Add method doc

--- LevelOne/MainIsHre.cs
+++ LevelOne/MainIsHre.cs
@@ -4,6 +4,10 @@ namespace LevelOne
 {
     public class MainIsHere
     {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
