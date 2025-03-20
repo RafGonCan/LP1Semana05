c71fc930a023fbff88571c81f84fa015c45bac22
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-20 22:17:48 +0000

    New method

--- LevelOne/MainIsHre.cs
+++ LevelOne/MainIsHre.cs
@@ -12,5 +12,13 @@ namespace LevelOne
         {
             Console.WriteLine("Hello LP1!");
         }
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main2(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
     }
 }
