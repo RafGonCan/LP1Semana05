2b0a419b8f3ea8e646707b489b6c0e8363f728e1
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-21 00:09:00 +0000

    Leveltwo proj

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Spectre.Console;
+using SoGoodLib;
+
+namespace LevelTwo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            SoGoodClass.SoGoodMethod();
+            CanvasImage image = new CanvasImage("tux.jpg");
+            image.MaxWidth(20);
+
+            if (args.Length >= 2)
+            {
+                image = new CanvasImage(args[0]);
+                image.MaxWidth(Convert.ToInt32(args[1]));
+            }
+            AnsiConsole.Write(image);
+        }
+    }
+}
---
+++ LevelTwo/SoGoodLib/SoGoodClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SoGoodLib
+{
+    public class SoGoodClass
+    {
+        public static string SoGoodMethod()
+        {
+        string sentence = "I came from another project!";
+        Console.WriteLine(sentence);
+        return sentence;
+        }
+    }
+}
